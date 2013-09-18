f2a1656806afb9e15c940a9085a8e4bb3ce3ae64
Simon Rozman <simon.rozman@amebis.si>
2013-09-18 07:47:21 +0000

    Verzijo sem nastavil na 1.3.

--- MSITSCA.h
+++ MSITSCA.h
@@ -6,14 +6,14 @@
 // Constants
 ////////////////////////////////////////////////////////////////////////////
 
-#define MSITSCA_VERSION       0x01020100
+#define MSITSCA_VERSION       0x01030000
 
 #define MSITSCA_VERSION_MAJ   1
-#define MSITSCA_VERSION_MIN   2
-#define MSITSCA_VERSION_REV   1
+#define MSITSCA_VERSION_MIN   3
+#define MSITSCA_VERSION_REV   0
 
-#define MSITSCA_VERSION_STR   "1.2.1"
-#define MSITSCA_VERSION_INST  "1.2.1.0"
+#define MSITSCA_VERSION_STR   "1.3"
+#define MSITSCA_VERSION_INST  "1.3.0.0"
 
 
 #if !defined(RC_INVOKED) && !defined(MIDL_PASS)
