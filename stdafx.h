7ca10a05a6e21d23f1e007b26f33ddd2fcc64925
Simon Rozman <simon.rozman@amebis.si>
2014-05-13 07:51:02 +0000

    Nastavitve konstant sem preselil v common.props.
    Verzijo sem nastavil na 1.0-pre3.

--- stdafx.h
+++ stdafx.h
@@ -4,25 +4,6 @@
 
 #pragma once
 
-#if defined(_UNICODE) && !defined(UNICODE)
-#define UNICODE
-#endif
-
-#if defined(_WIN32) && !defined(WIN32)
-#define WIN32
-#endif
-
-#ifndef STRICT
-#define STRICT
-#endif
-
-#define VC_EXTRALEAN                       // Exclude rarely-used stuff from Windows headers
-#define _WIN32_WINNT 0x0501                // Include Windows XP symbols
-#define _WINSOCKAPI_                       // Prevent inclusion of winsock.h in windows.h
-//#define _ATL_APARTMENT_THREADED
-//#define _ATL_NO_AUTOMATIC_NAMESPACE
-//#define _ATL_CSTRING_EXPLICIT_CONSTRUCTORS // Some CString constructors will be explicit
-
 #include <atlbase.h>
 #include <atlfile.h>
 #include <atlstr.h>
