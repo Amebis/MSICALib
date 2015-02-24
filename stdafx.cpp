0f226a9de5b6410e86333db9bb2cd4cde13f7011
Simon Rozman <simon.rozman@amebis.si>
2015-02-24 10:51:24 +0000

    Copyright notice added.

--- stdafx.cpp
+++ stdafx.cpp
@@ -1,1 +1,23 @@
+/*
+    Setup, Copyright (C) Amebis
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+
+    See the GNU General Public License for more details, included in the file
+    LICENSE.rtf which you should have received along with this program.
+
+    If you did not receive a copy of the GNU General Public License,
+    write to the Free Software Foundation, Inc., 675 Mass Ave,
+    Cambridge, MA 02139, USA.
+
+    Amebis can be contacted at http://www.amebis.si
+*/
+
 #include "stdafx.h"
