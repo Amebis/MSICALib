37479173a2c811d463934a9c8cec4ac2fa7fd142
Simon Rozman <simon@rozman.si>
2016-09-19 09:57:02 +0200

    Adaptation to MSIBuild updates - Source file English "en_US" instead of generic "En" now - MSIBUILD_LANG_PO merged to LANG

--- MSIBuild/build_all.bat
+++ MSIBuild/build_all.bat
@@ -1,9 +1,5 @@
 @echo off
-nmake.exe /ls All LANG=En CFG=Debug   PLAT=Win32
-nmake.exe /ls All LANG=Sl CFG=Debug   PLAT=Win32
-nmake.exe /ls All LANG=En CFG=Debug   PLAT=x64
-nmake.exe /ls All LANG=Sl CFG=Debug   PLAT=x64
-nmake.exe /ls All LANG=En CFG=Release PLAT=Win32
-nmake.exe /ls All LANG=Sl CFG=Release PLAT=Win32
-nmake.exe /ls All LANG=En CFG=Release PLAT=x64
-nmake.exe /ls All LANG=Sl CFG=Release PLAT=x64
+nmake.exe /ls All LANG=en_US CFG=Debug   PLAT=Win32
+nmake.exe /ls All LANG=en_US CFG=Debug   PLAT=x64
+nmake.exe /ls All LANG=en_US CFG=Release PLAT=Win32
+nmake.exe /ls All LANG=en_US CFG=Release PLAT=x64
--- MSIBuild/clean_all.bat
+++ MSIBuild/clean_all.bat
@@ -1,9 +1,5 @@
 @echo off
-nmake.exe /ls Clean LANG=En CFG=Debug   PLAT=Win32
-nmake.exe /ls Clean LANG=Sl CFG=Debug   PLAT=Win32
-nmake.exe /ls Clean LANG=En CFG=Debug   PLAT=x64
-nmake.exe /ls Clean LANG=Sl CFG=Debug   PLAT=x64
-nmake.exe /ls Clean LANG=En CFG=Release PLAT=Win32
-nmake.exe /ls Clean LANG=Sl CFG=Release PLAT=Win32
-nmake.exe /ls Clean LANG=En CFG=Release PLAT=x64
-nmake.exe /ls Clean LANG=Sl CFG=Release PLAT=x64
+nmake.exe /ls Clean LANG=en_US CFG=Debug   PLAT=Win32
+nmake.exe /ls Clean LANG=en_US CFG=Debug   PLAT=x64
+nmake.exe /ls Clean LANG=en_US CFG=Release PLAT=Win32
+nmake.exe /ls Clean LANG=en_US CFG=Release PLAT=x64
